0ad26b352f7f8fae1acca12884ca75186daf16e9
ShahadAlshahrani <139855183+ShahadAlshahrani@users.noreply.github.com>
2023-08-11 11:30:57 +0300

    Add files via upload

---
+++ path.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$butn = $_POST['butn1'];
+
+$sql = "INSERT INTO path11 (butn) VALUES ('$butn')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
